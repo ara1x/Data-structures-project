d11a33c9444887980a3c4f0e118cbd127f71920e
ara1x <Aroub1414@hotmail.com>
2025-11-03 20:28:12 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/datastructures/DataStructures.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datastructures;
+
+/**
+ *
+ * @author aroub
+ */
+
+
+public class DataStructures {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+
+    
+    }
+}
+    
+
